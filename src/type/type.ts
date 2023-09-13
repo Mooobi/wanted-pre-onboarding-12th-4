16105c4db48aa2f2aa0e64c2f5f6913975e86e64
Mooobi <antod123@naver.com>
2023-09-13 16:46:07 +0900

    Chore: 타입 수정

--- src/type/type.ts
+++ src/type/type.ts
@@ -1,9 +1,16 @@
-export type graph = {
+export type data = {
   id: string;
   value_area: number;
   value_bar: number;
 };
 
-export type graphList = {
-  [timestamp: string]: graph;
+export type dataList = {
+  [timestamp: string]: data;
+};
+
+export type prcessedData = {
+  time: string;
+  id: string;
+  value_bar: number;
+  value_area: number;
 };
